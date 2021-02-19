89d527f2fd750139539b0e53b1d601a51b7818c3
itzhaolei <itzhaolei@foxmail.com>
2021-02-19 10:40:27 +0800

    修复问题

---
+++ Example/Pods/Target Support Files/ZLHTTPSessionManager/ZLHTTPSessionManager-umbrella.h
@@ -0,0 +1,17 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#else
+#ifndef FOUNDATION_EXPORT
+#if defined(__cplusplus)
+#define FOUNDATION_EXPORT extern "C"
+#else
+#define FOUNDATION_EXPORT extern
+#endif
+#endif
+#endif
+
+#import "ZLHTTPSessionHeader.h"
+
+FOUNDATION_EXPORT double ZLHTTPSessionManagerVersionNumber;
+FOUNDATION_EXPORT const unsigned char ZLHTTPSessionManagerVersionString[];
+
