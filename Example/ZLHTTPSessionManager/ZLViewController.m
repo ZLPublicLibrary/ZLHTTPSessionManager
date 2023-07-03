de5928b421da049d1bf70befedcdb9fc2f6b9d65
itzhaolei <itzhaolei@foxmail.com>
2023-07-03 15:34:26 +0800

    优化

--- Example/ZLHTTPSessionManager/ZLViewController.m
+++ Example/ZLHTTPSessionManager/ZLViewController.m
@@ -33,8 +33,26 @@
     // Dispose of any resources that can be recreated.
 }
 
-#pragma mark - 请求示例
+#pragma mark - 普通请求示例
 - (void)request {
+    [ZLHTTPSessionManager request:@"/appsys/sitelist.go" HTTPMethod:post Params:nil AddHttpHeader:false CachePolicy:false downloadProgress:nil success:^(NSURLSessionDataTask * _Nonnull task, id  _Nullable responseObject) {
+        
+    } failure:^(NSURLSessionDataTask * _Nonnull task, NSError * _Nullable error) {
+        
+    }];
+}
+
+#pragma mark - 下载示例
+- (void)download {
+    [ZLHTTPSessionManager request:@"/appsys/sitelist.go" HTTPMethod:post Params:nil AddHttpHeader:false CachePolicy:false downloadProgress:nil success:^(NSURLSessionDataTask * _Nonnull task, id  _Nullable responseObject) {
+        
+    } failure:^(NSURLSessionDataTask * _Nonnull task, NSError * _Nullable error) {
+        
+    }];
+}
+
+#pragma mark - 上传示例
+- (void)upload {
     [ZLHTTPSessionManager request:@"/appsys/sitelist.go" HTTPMethod:post Params:nil AddHttpHeader:false CachePolicy:false downloadProgress:nil success:^(NSURLSessionDataTask * _Nonnull task, id  _Nullable responseObject) {
         
     } failure:^(NSURLSessionDataTask * _Nonnull task, NSError * _Nullable error) {
